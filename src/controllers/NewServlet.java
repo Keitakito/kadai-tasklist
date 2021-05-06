5a17a31b92e2b64dec0a2e319c84cf58b1c82dbe
akito takeda <kadokei831@icloud.com>
2021-05-06 14:33:01 +0900

    NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,17 +1,12 @@
 package controllers;
 
-import java.io.IOException;
-import java.sql.Timestamp;
-
-import javax.persistence.EntityManager;
-import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -31,27 +26,12 @@ public class NewServlet extends HttpServlet {
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+        request.setAttribute("_token", request.getSession().getId());
 
-        Task t = new Task();
+        request.setAttribute("task", new Task());
 
-        String content = "お腹すいた";
-        t.setContent(content);
-
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-        t.setCreated_at(currentTime);
-        t.setUpdated_at(currentTime);
-
-        // データベースに保存
-        em.getTransaction().begin();
-        em.persist(t);
-        em.getTransaction().commit();
-
-        // 自動採番されたIDの値を表示
-        response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
-        em.close();
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
     }
 }
